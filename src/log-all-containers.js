506172860c78db8fee7b4b241dfde17d6725c8a4
Mick Staugaard <mick@staugaard.com>
2017-12-15 17:18:06 -0800

    Allow specifying a pattern to match against container names

--- src/log-all-containers.js
+++ src/log-all-containers.js
@@ -4,15 +4,18 @@ const containerLogs = require('./container-logs')
 
 let watchingContainers = {}
 
-module.exports = function logAllContainers({ tail, useColor }) {
+module.exports = function logAllContainers({ tail, useColor, pattern }) {
+  let patternRegex = pattern ? new RegExp(pattern) : new RegExp()
+
   docker.listContainers({ all: false }, (err, containers) => {
     if (containers) {
       containers.map(container => {
-        if (!watchingContainers[container.Id]) {
+        let containerName = container.Names[0].replace(/^\//, '')
+        if (!watchingContainers[container.Id] && patternRegex.test(containerName)) {
           watchingContainers[container.Id] = true
           containerLogs(
             docker.getContainer(container.Id),
-            container.Names[0].replace(/^\//, ''),
+            containerName,
             { tail, useColor }
           )
         }
